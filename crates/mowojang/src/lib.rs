f0d9028d55e61b0c755dbb3c63213eb48966d45e
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2024-02-01 21:48:51 -0800

    nyaaa~

--- crates/mowojang/src/lib.rs
+++ crates/mowojang/src/lib.rs
@@ -1,6 +1,21 @@
 use database::player::PlayerInfo;
 use uuid::Uuid;
 
+pub fn valid_java_username(username: &str) -> bool {
+    (3..=16).contains(&username.len())
+        && username
+            .chars()
+            .all(|char| (char.is_ascii_alphanumeric() || char == '_'))
+}
+
+pub fn valid_geyser_username(username: &str) -> bool {
+    let mut chars = username.chars();
+    (3..=12).contains(&username.len().saturating_sub(1))
+        && chars.next() == Some('.')
+        && chars.next().expect("wha").is_ascii_alphabetic()
+        && chars.all(|char| (char.is_ascii_alphanumeric() || char == '_'))
+}
+
 pub async fn check_uuid(uuid: Uuid) -> Option<PlayerInfo> {
     match reqwest::get(format!(
         "https://mowojang.matdoes.dev/{}",
